ff746d685ae85f148ebd14ecfdd4eaddf3d9a961
Rilson Almeida <rilson.almeida@gmail.com>
2021-09-06 14:53:58 -0300

    Basic static components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css'
 import App from './App';
+import NavBar from 'components/NavBar';
 
 
 ReactDOM.render(
