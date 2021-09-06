17706aeb054d273e09542b50fa344bb0f14a7e06
Rilson Almeida <rilson.almeida@gmail.com>
2021-09-06 15:45:15 -0300

    Fix error message in Netlify deploy

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css'
 import App from './App';
-import NavBar from 'components/NavBar';
-
 
 ReactDOM.render(
   <React.StrictMode>
