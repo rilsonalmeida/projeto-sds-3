73864fcac3c1672c82d78b86a320351864eb52f6
Rilson Almeida <rilson.almeida@gmail.com>
2021-09-06 15:31:14 -0300

    Static charts (Apexcharts)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -7,7 +9,23 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Benvindo</h1>
+        <h1 className="text-primary py-3">Painel de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Vendas aprovadas(%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Total de Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py3 text-primary">
+          <h2>Total de negociações</h2>
+        </div>
+
         <DataTable />
       </div>
       <Footer />
