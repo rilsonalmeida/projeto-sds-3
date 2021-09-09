28a8568f5866518b93bcbc09ebf73a55a858135e
Rilson Almeida <rilson.almeida@gmail.com>
2021-09-08 21:40:42 -0300

    Implement layers

---
+++ backend/src/main/java/com/pulsartrends/dsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.pulsartrends.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pulsartrends.dsvendas.dto.SellerDTO;
+import com.pulsartrends.dsvendas.entities.Seller;
+import com.pulsartrends.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
