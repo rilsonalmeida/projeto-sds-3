7244c3e32bb816a826d94f5922020a9e91d99f2a
Rilson Almeida <rilson.almeida@gmail.com>
2021-09-08 22:11:44 -0300

    Pagination

---
+++ backend/src/main/java/com/pulsartrends/dsvendas/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.pulsartrends.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pulsartrends.dsvendas.dto.SaleDTO;
+import com.pulsartrends.dsvendas.entities.Sale;
+import com.pulsartrends.dsvendas.repositories.SaleRepository;
+import com.pulsartrends.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
