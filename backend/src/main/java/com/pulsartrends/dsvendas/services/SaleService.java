4935a0d3174acc808854f3f158a0482396f46309
Rilson Almeida <rilson.almeida@gmail.com>
2021-09-08 22:58:23 -0300

    Group by search

--- backend/src/main/java/com/pulsartrends/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/pulsartrends/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.pulsartrends.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pulsartrends.dsvendas.dto.SaleDTO;
+import com.pulsartrends.dsvendas.dto.SaleSuccessDTO;
+import com.pulsartrends.dsvendas.dto.SaleSumDTO;
 import com.pulsartrends.dsvendas.entities.Sale;
 import com.pulsartrends.dsvendas.repositories.SaleRepository;
 import com.pulsartrends.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,13 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}	
 
 }
